4ad770f3ca57a8594c29b92da75b5a59a2b426df
moises serrano <moycs777@gmail.com>
2023-09-04 12:38:22 -0300

    [FEAT] Creataing factory for String Value Object

--- src/shared/domain/value_object/string_value_object.ts
+++ src/shared/domain/value_object/string_value_object.ts
@@ -1,3 +1,17 @@
-import { Value_object } from "./value_object";
+import {Value_object} from "./value_object";
 
-export abstract class StringValueObject extends Value_object<string> {}
+export abstract class StringValueObject extends Value_object<string> {
+  constructor(readonly value: any) {
+    super(value);
+  }
+
+  static create(value: string): StringValueObject {
+    return new ConcreteStringValueObject(value);
+  }
+}
+
+class ConcreteStringValueObject extends StringValueObject {
+  constructor(value: string) {
+    super(value);
+  }
+}
