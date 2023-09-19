232aa1fe87e6359eecd176642aacb61690967435
Moises Serrano <moycs777@gmail.com>
2023-09-19 15:37:25 -0300

    [WIP] Exchange integrator response

--- src/exchange/domain/interfaces/exchange_integrator_service.interface.ts
+++ src/exchange/domain/interfaces/exchange_integrator_service.interface.ts
@@ -1,8 +1,9 @@
 import { ExchangeMarketPayload } from "../types/exchange_market_payload.type";
 import { Exchange } from "../exchange";
+import {ExchangeRequestResponse} from "../types/exchange_response.type";
 
 export interface IExchangeIntegratorService {
-  createExchange(exchangePayload: ExchangeMarketPayload): Promise<Exchange>;
+  createExchange(exchangePayload: ExchangeMarketPayload): Promise<ExchangeRequestResponse>;
 
-  acceptExchange(exchange: Exchange): Promise<Exchange>;
+  acceptExchange(exchange: Exchange): Promise<ExchangeRequestResponse>;
 }
