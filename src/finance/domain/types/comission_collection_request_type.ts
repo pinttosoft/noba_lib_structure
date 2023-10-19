f4fb70cb57489c876a5b77df337e5a0c1803b342
abejarano <programador.angel@gmail.com>
2023-10-19 12:05:52 -0300

    fix: nuevo tipo

---
+++ src/finance/domain/types/comission_collection_request_type.ts
@@ -0,0 +1,7 @@
+import {TypeFinancialMovement} from "../enums/type_financial_movement.enum";
+
+export type ComissionCollectionRequestType = {
+    withdrawalId: string;
+    typeFinancialMovement: TypeFinancialMovement;
+    transactionId: string;
+}
--- src/finance/index.ts
+++ src/finance/index.ts
@@ -2,11 +2,11 @@ export { FinancialMovementStatus } from "./domain/enums/financial_movement_statu
 
 export { TypeFinancialMovement } from "./domain/enums/type_financial_movement.enum";
 
-export { FinancialMovement } from "./domain/types/financial_movement_type";
 export { Finance } from "./domain/finance";
 
 export { IFinanceRepository } from "./domain/interfaces/finance_repository.interface";
 
 export { FinanceMongoRepository } from "./infrastructure/mongo/finance_mongo_repository";
 
-export {} from "./domain/types/";
+export { ComissionCollectionRequestType } from "./domain/types/comission_collection_request_type";
+export { FinancialMovement } from "./domain/types/financial_movement_type";
