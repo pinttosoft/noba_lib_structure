1d9977a85304080a47bdf9f7af7f5adf8ded4595
Moises Serrano <moycs777@gmail.com>
2023-11-14 19:22:18 -0300

    [WIP] Adding interface for layer 2 client/applications

--- src/client/domain/interfaces/client_service.interface.ts
+++ src/client/domain/interfaces/client_service.interface.ts
@@ -1,9 +1,10 @@
 import { Client } from "../client";
 import { ApplicationUpdateType } from "../types/application_update.type";
+import { IClient } from "./client.interface";
 
 export interface IClientBankingServiceInterface {
   accountOpening(client: Client): Promise<void>;
-  createApplication(client: Client): Promise<string>;
+  createApplication(client: IClient): Promise<string>;
   retrieveApplication(applicationId: string): Promise<string>;
   statusApplication(applicationId: string): Promise<string>;
   updateApplication(
