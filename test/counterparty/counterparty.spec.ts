a1d6d0ba3f2d7ff7ecc6320392c1dbac467b4caf
johanna <lanza.johanna@gmail.com>
2023-12-08 09:52:45 -0400

    validacion con expect y correccion

--- test/counterparty/counterparty.spec.ts
+++ test/counterparty/counterparty.spec.ts
@@ -40,7 +40,31 @@ describe("Counterparty", () => {
     //   CounterpartyFactory.createNewCounterparty(counterpartyDTO);
   });
 
+  it("should list used criteria method", async () => {
+    const clientId = "FSilva187263254";
 
+    const filterClientId: Map<string, string> = new Map([
+      ["field", "clientId"],
+      ["operator", Operator.EQUAL],
+      ["value", clientId],
+    ]);
+
+    const filterCounterpartyType: Map<string, string> = new Map([
+      ["field", "counterpartyType"],
+      ["operator", Operator.EQUAL],
+      ["value", CounterpartyType.CRYPTO],
+    ]);
+
+    const criteria = new Criteria(
+      Filters.fromValues([filterClientId, filterCounterpartyType]),
+      Order.fromValues("createdAt", OrderTypes.DESC),
+      20,
+      1,
+    );
+
+    const result = await CounterpartyMongoRepository.instance().list(criteria);
+    console.log(result);
+  });
 
   it("should register or search counterparty internal", async () => {
     const clientOrigin =
