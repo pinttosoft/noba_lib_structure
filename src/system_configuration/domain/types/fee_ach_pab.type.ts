1d5565db2b35296fb324994264aa6577575bf6ea
Moises Serrano <moycs777@gmail.com>
2024-01-25 16:54:00 -0300

    wip: fee ach pab, system configuration

---
+++ src/system_configuration/domain/types/fee_ach_pab.type.ts
@@ -0,0 +1,10 @@
+export type FeeACHPAB = {
+  domestic: {
+    in: number;
+    out: number;
+  };
+  international: {
+    in: number;
+    out: number;
+  };
+};
--- test/system_config/system_config.spec.ts
+++ test/system_config/system_config.spec.ts
@@ -4,5 +4,14 @@ describe("System config", () => {
   it("feeWire", async () => {
     const feeWire =
       await SystemConfigurationMongoRepository.instance().getDefaultFeeWire();
+
+    console.log("feeWire", feeWire);
+  });
+
+  it("Should get fees for ACH Panama", async () => {
+    const feeACHPAB =
+      await SystemConfigurationMongoRepository.instance().getDefaultFeeACHPAB();
+
+    console.log("feeACHPAB", feeACHPAB);
   });
 });
