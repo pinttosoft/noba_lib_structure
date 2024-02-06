42def1b7a07084ac385bcda64b2e54076aa6a69d
jlanza1981 <lanza.johanna@gmail.com>
2024-02-06 17:32:59 -0400

    FEAT: correcion instrucciones swift

--- src/banking/domain/types/instruction_deposit_fiat.type.ts
+++ src/banking/domain/types/instruction_deposit_fiat.type.ts
@@ -30,28 +30,28 @@ export type InstructionDepositFiat = {
     memo: string;
   };
   SWIFT:{
-    "instruction_type": string;
-    "account_holder_name": string;
-    "account_number": string;
-    "swift_bic": string;
-    "account_holder_address": {
-      "address_line1": string;
-      "address_line2": string;
-      "city": string;
-      "state": string;
-      "postal_code": string;
-      "country_code": string;
+    instruction_type: string;
+    account_holder_name: string;
+    account_number: string;
+    swift_bic: string;
+    account_holder_address: {
+      address_line1: string;
+      address_line2: string;
+      city: string;
+      state: string;
+      postal_code: string;
+      country_code: string;
     },
-    "institution_name": string;
-    "institution_address": {
-      "address_line1": string;
-      "address_line2": string;
-      "city": string;
-      "state": string;
-      "postal_code": string;
-      "country_code": string;
+    institution_name: string;
+    institution_address: {
+      address_line1: string;
+      address_line2: string;
+      city: string;
+      state: string;
+      postal_code: string;
+      country_code: string;
     },
-    "asset_type_id": string;
-    "memo": string;
+    asset_type_id: string;
+    memo: string;
   };
 };
