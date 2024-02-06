114623f3b87822175f5d40013321fc2f88896758
jlanza1981 <lanza.johanna@gmail.com>
2024-02-06 14:00:08 -0400

    FEAT: se agrego instrucciones swift

--- src/banking/domain/types/instruction_deposit_fiat.type.ts
+++ src/banking/domain/types/instruction_deposit_fiat.type.ts
@@ -28,5 +28,30 @@ export type InstructionDepositFiat = {
     accountNumber: string;
     accountRoutingNumber: string;
     memo: string;
+  }
+  SWIFT:{
+    "instruction_type": string,
+    "account_holder_name": string,
+    "account_number": string,
+    "swift_bic": string,
+    "account_holder_address": {
+      "address_line1": string,
+      "address_line2": string,
+      "city": string,
+      "state": string,
+      "postal_code": string,
+      "country_code": string
+    },
+    "institution_name": string,
+    "institution_address": {
+      "address_line1": string,
+      "address_line2": string,
+      "city": string,
+      "state": string,
+      "postal_code": string,
+      "country_code": string
+    },
+    "asset_type_id": string,
+    "memo": string
   };
 };
