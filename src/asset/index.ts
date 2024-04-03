3cbd953f1152deda91ddad5bbdc1d903558066bd
Moises Serrano <moycs777@gmail.com>
2024-04-03 14:20:24 -0300

    wip: when doing some internal transfer, if the destination wallet not exists, then create it

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -5,7 +5,9 @@ export { WalletInformationDTO } from "./domain/types/wallet_information.type";
 export { CounterpartyAsset } from "./domain/counterparty_asset";
 
 export { IAssetRepository } from "./domain/interfaces/asset_repository.interface";
+
 export { Asset } from "./domain/asset";
 export { AssetMongoRepository } from "./infrastructure/mongo/asset_mongo_repository";
-
 export { AssetNotFound } from "./domain/exceptions/asset_not_found";
+
+export { AssetCodes } from "./domain/enums/asset_codes.enum";
