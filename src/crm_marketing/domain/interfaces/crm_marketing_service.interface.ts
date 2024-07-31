000c7a57e634571237b74839cc350acaf5d17581
wanervalencia <valenciawaner@gmail.com>
2024-07-31 10:25:33 -0500

    hubspot crm integration

---
+++ src/crm_marketing/domain/interfaces/crm_marketing_service.interface.ts
@@ -0,0 +1,38 @@
+import { CrmMarketingContact } from "../types/crm_marketing_contact.type";
+
+export interface ICrmMarketingService {
+  createContact(contact: CrmMarketingContact): Promise<{
+    id: string;
+  }>;
+
+  getContactByEmail(email: string): Promise<CrmMarketingContact>;
+
+  addContactToList(contactId: number, listId: number): Promise<void>;
+
+  getOrCreateContactByEmail(
+    email: string,
+    contact?: CrmMarketingContact,
+  ): Promise<{ id: string }>;
+
+  addNewContactToList(
+    contact: CrmMarketingContact,
+    listId: number,
+  ): Promise<{
+    id: string;
+  }>;
+
+  updateContactByEmail(
+    email: string,
+    contact: CrmMarketingContact,
+  ): Promise<void>;
+
+  removeContactFromList(contactId: number, listId: number): Promise<void>;
+
+  transferContactBetweenListsByEmail(
+    email: string,
+    fromListId: number,
+    toListId: number,
+  ): Promise<void>;
+
+  removeContactFromListByEmail(email: string, listId: number): Promise<void>;
+}
